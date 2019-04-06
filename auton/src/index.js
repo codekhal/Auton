25496320d582e770d74d37b5d1a5c9f3d1f5f059
khushal <vyas.khushal1999@gmail.com>
2019-04-06 20:19:13 +0530

    Starting files

---
+++ auton/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Hello from './Hello';
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+
+ReactDOM.render(<Hello/>, document.getElementById('root'));
+
+
+serviceWorker.unregister();
