9a0295aa8adba9afc868473ac9138bb4fb22ada4
khushal <vyas.khushal1999@gmail.com>
2019-04-15 12:35:20 +0530

    added final files

--- auton/src/Scroll.js
+++ auton/src/Scroll.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Scroll = (props) => {
     return (
         <div style = {{overflow: 'scroll', border: '7px solid black', height: '700px'}}> 
-            {props.chidren}
+            {props.children}
         </div>
     );
 };
